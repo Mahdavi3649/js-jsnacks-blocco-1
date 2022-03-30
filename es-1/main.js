7ae3a9ded6022b36f7919abfe11fab682a80303b
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-30 10:35:29 +0200

    modificato e messo in una cartella

---
+++ es-1/main.js
@@ -0,0 +1,14 @@
+
+const first_number = parseInt(prompt("type your first number"))
+const second_number = parseInt(prompt("type your second number"))
+
+console.log(first_number , second_number);
+
+if(first_number > second_number){
+    console.log("first number is biggar than second");
+} else if(first_number < second_number){
+    console.log("second number is biggar than first");
+} else{
+    console.log("Equal");
+
+}
