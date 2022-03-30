c3bf2c41b1a0ecb76348a694784c193f9d0b87f1
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-30 17:56:37 +0200

    Done!

--- es-8/main.js
+++ es-8/main.js
@@ -1,10 +1,12 @@
 /* Chiedi un numero di 4 cifre all’utente
 e calcola la somma di tutte le cifre che compongono il numero. */
 
+
 const numUtente = prompt("Insert the 4-digit number")
 console.log(numUtente);
 
 let result = 0;
+
 if (isNaN(numUtente)){
     alert("The text entered obligatorily must be a number");
 
@@ -20,7 +22,5 @@ if (isNaN(numUtente)){
         result += (parseInt(numUtente[i]));
     }
      console.log("The sum of all digits the number you've entered is: " + result);
-
-
 }
 
