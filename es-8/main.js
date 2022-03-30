f63c9c5ffb92b7dc1b0e45db22011055da693764
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-30 17:21:45 +0200

    ?!

---
+++ es-8/main.js
@@ -0,0 +1,26 @@
+/* Chiedi un numero di 4 cifre all’utente
+e calcola la somma di tutte le cifre che compongono il numero. */
+
+const numUtente = parseInt(prompt("Insert the 4-digit number"))
+console.log(numUtente);
+
+let result = 0;
+if (isNaN(numUtente)){
+    alert("The text entered obligatorily must be a number");
+
+} else if(numUtente.length > 4){
+    alert("The number you entered has more than 4 digits");
+
+}else if(numUtente.length < 4){
+    alert("The number you entered has less than 4 digits");
+
+}else{
+
+    for (let i = 0; i < numUtente.length; i++) {
+        result += (numUtente[i]);
+    }
+     console.log("The sum of all digits the number you've entered is: " + result);
+
+
+}
+
