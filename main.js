1209158cc5ec2c8303ec28b9ef3469a38a9c3668
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-30 10:22:09 +0200

    Done

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const first_number = parseInt(prompt("type your first number"))
+const second_number = parseInt(prompt("type your second number"))
+
+console.log(first_number , second_number);
+
+if(first_number > second_number){
+    console.log("first number is biggar than second");
+} else{
+    console.log("second number is biggar than first");
+
+}
