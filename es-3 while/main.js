512ec501964a2db2419c55b0aeba1a15d14fceb0
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-31 11:50:19 +0200

    Done!

---
+++ es-3 while/main.js
@@ -0,0 +1,19 @@
+
+/* SNACK 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+
+
+let number;
+let somma = 0
+let i;
+while(i < 10){
+    let number = parseInt(prompt("Type un numbers")); 
+    somma = somma + number
+    i++
+}
+
+console.log(somma);
+
+
